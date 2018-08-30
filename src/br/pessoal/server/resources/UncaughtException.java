a5bc782b87bb04c9e0718acee2d64631f17959cd
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2018-08-30 11:08:47 -0300

    Finalizado a criacao da exception personalizada

---
+++ src/br/pessoal/server/resources/UncaughtException.java
@@ -0,0 +1,19 @@
+package br.pessoal.server.resources;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class UncaughtException extends Throwable implements ExceptionMapper<Throwable>{
+
+	private static final long serialVersionUID  = 1L;
+
+	@Override
+	public Response toResponse(Throwable exception) {		
+		return Response.status(500).entity("Ocorreu um erro, tente novamente mais tarde").type("text/plain").build();
+	}
+	
+	
+
+}
